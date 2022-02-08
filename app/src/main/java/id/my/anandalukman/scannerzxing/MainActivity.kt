53c653e5ddc0df9b800dbc556369683684b2e8f5
Ananda Lukman <ananda.muhamad.lukman@gmail.com>
2022-02-08 21:06:45 +0700

    Initial commit

---
+++ app/src/main/java/id/my/anandalukman/scannerzxing/MainActivity.kt
@@ -0,0 +1,48 @@
+package id.my.anandalukman.scannerzxing
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.google.zxing.integration.android.IntentIntegrator
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var btnBarcode: Button
+    lateinit var textView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        title = "LukmanTestScan"
+        btnBarcode = findViewById(R.id.button)
+        textView = findViewById(R.id.txtContent)
+        btnBarcode?.setOnClickListener {
+            val intent = IntentIntegrator(this@MainActivity)
+            intent.setBeepEnabled(false)
+            intent.setCameraId(0)
+            intent.setPrompt("SCAN")
+            intent.setBarcodeImageEnabled(false)
+            intent.initiateScan()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "Dibatalin", Toast.LENGTH_SHORT).show()
+            } else {
+                Log.d("TestScan", "Discan!!!")
+                Toast.makeText(this, "HasilScan : ${result.contents}", Toast.LENGTH_SHORT).show()
+                textView.text = String.format("Hasil scan adalah : $result")
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+}
